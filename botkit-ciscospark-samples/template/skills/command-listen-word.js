b8d904f41dd5a5077c0e7dfa323165bd8171cc54
AntoineOrsoni <ant.orsoni@gmail.com>
2018-01-23 09:23:12 +0100

    POST using JS

--- botkit-ciscospark-samples/template/skills/command-listen-word.js
+++ botkit-ciscospark-samples/template/skills/command-listen-word.js
@@ -5,5 +5,33 @@ module.exports = function (controller) {
         var text = "I heard : ";
         text += name + " !";
         bot.reply(message, text);
+
+
+
+        // Sending the POST
+
+        var data = JSON.stringify({
+            "name": name
+        });
+
+        var url = "http://127.0.0.1:5000";
+
+
+        var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+              if (this.readyState === 4) {
+                console.log(this.responseText);
+              }
+        });
+
+        xhr.open("POST", url);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+
+        xhr.send(data);
     });
 }
